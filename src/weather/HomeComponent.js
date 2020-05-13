204c3e2f739da392094f67f5768f0833a711bc19
Vignesh <60935095+Vignesh2308@users.noreply.github.com>
2020-05-13 17:06:55 +0530

    Update HomeComponent.js
    added break statement

--- src/weather/HomeComponent.js
+++ src/weather/HomeComponent.js
@@ -10,6 +10,7 @@ class Home extends React.Component{
         return(
             <React.Fragment>
                 <div id='container'>
+                <br /><br /> <br />
                 <Header />
                 <Search />
                 <Footer />
