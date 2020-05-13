89114262d31a089bedbd91a6af51920a9a5cca38
Vignesh <60935095+Vignesh2308@users.noreply.github.com>
2020-05-13 17:06:11 +0530

    Update HeaderComponent.js
    added classname to fixed top

--- src/weather/HeaderComponent.js
+++ src/weather/HeaderComponent.js
@@ -3,7 +3,7 @@ import React from 'react'
 function Header(){
     return(
         <React.Fragment>
-                <nav className="navbar navbar-light " id='header'>
+                <nav className="navbar navbar-light fixed-top " id='header'>
                     <a className="navbar-brand" href=".">
                         <img src="logo192.png" width="30" height="30" className="d-inline-block align-top" alt="Logo" />&nbsp;
                         <strong className='text-primary'>The Weather Forecast</strong>
