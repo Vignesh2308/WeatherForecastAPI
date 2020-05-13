d50880639301ba843831972fa438e2210314160c
Vignesh <60935095+Vignesh2308@users.noreply.github.com>
2020-05-13 17:05:20 +0530

    Update FooterComponent.js
    added classname to fixed bottom

--- src/weather/FooterComponent.js
+++ src/weather/FooterComponent.js
@@ -3,7 +3,7 @@ import React from 'react'
 function Footer(){
     return(
         <React.Fragment>
-            <div id='foot'>
+            <div id='foot' className='fixed-bottom'>
             <strong>Developed By Vignesh</strong> &nbsp; &nbsp; 
             <a href='https://www.facebook.com/vignesh.parker.7/'>
                 <img className='icon' src='https://i.pinimg.com/originals/b7/63/69/b763699fd1fa3bfb374442593ae642e1.png' width="30" height="30" alt='fb'></img></a>
